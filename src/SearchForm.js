b5f578a105f41316ed0743ba1644d9eec1f950a7
MaQueen <shusineko@icloud.com>
2021-01-02 12:58:52 +0900

    searchForm/Create

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -3,11 +3,16 @@ import { useGlobalContext } from "./context";
 import { HANDLE_SEARCH } from "./actions";
 
 const SearchForm = () => {
-  const { query, HANDLE_SEARCH } = useGlobalContext();
+  const { query, handleSearch } = useGlobalContext();
   return (
     <form className="search-form" onSubmit={(e) => e.preventDefault()}>
       <h2>search hacker news</h2>
-      <input type="text" className="form-input" value={query} />
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
     </form>
   );
 };
