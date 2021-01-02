94d86f31600eb0430ec907d6b3cc61af16f5b0ab
MaQueen <shusineko@icloud.com>
2021-01-02 12:19:42 +0900

    removeStory

--- src/context.js
+++ src/context.js
@@ -39,7 +39,7 @@ const AppProvider = ({ children }) => {
   };
 
   const removeStory = (id) => {
-    console.log(id);
+    dispatch({ type: REMOVE_STORY, payload: id });
   };
 
   useEffect(() => {
